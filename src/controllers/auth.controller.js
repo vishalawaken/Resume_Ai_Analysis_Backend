ed031ac349c313214db49b5cd9afc6bbb7e78218
vishalawaken <tsinghvishall@gmail.com>
2026-04-27 16:41:26 +0530

    refactor: enhance cookie settings for token in authentication controllers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,7 +42,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 24 * 60 * 60 * 1000
+    })
 
 
     res.status(201).json({
@@ -88,7 +93,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 24 * 60 * 60 * 1000
+    })
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
